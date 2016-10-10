346e2f6e3df427f50e3230c5bbb367964029161a
lizaKonovalova <liza_konovalova_2000234@mail.ru>
2016-10-10 22:00:31 +0300

    С++ АЛГОРИТМ ЕВКЛИДА

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-// Алгоритм Евклида нахождения НОД
-int gcd ( int a , int b )
-{
-    if ( b == 0 )
-     return a ;
+#include <iostream>
+
+using namespace std;
+
+int NOD( int a , int b )
+{ if ( b==0 )
+    return a ;
     else
-        return gcd ( b , a % b ) ;
+        return NOD ( b , a % b )
+}
+int NOK ( int a , int b ) {
+return ( a * b / NOD (a , b) );
+}
+int main ( ) {
+int a , b ;
+cout << "a=" ;
+cin >> a ;
+cout << "b="
+cin  >> b ;
+cout << NOK ( a , b ) ;
+cin.get(); cin.get( ) ;
+return 0
 }
