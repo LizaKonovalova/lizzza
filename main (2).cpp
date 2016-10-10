0a4c5552a07233e91e060a66ee6c551f7ca79431
lizaKonovalova <liza_konovalova_2000234@mail.ru>
2016-10-10 22:23:48 +0300

    .....

---
+++ main (2).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int NOD( int a , int b )
+{ if ( b==0 )
+    return a ;
+    else
+        return NOD ( b , a % b ) ; }
+int NOK ( int a , int b ) {
+return ( a * b / NOD (a , b) );
+}
+int main ( ) {
+int a , b ;
+cout << "a=" ;
+cin >> a ;
+cout << "b=" ;
+cin >> b;
+cout << NOK ( a , b ) ;
+cin.get(); cin.get() ;
+return 0 ;
+}
+
