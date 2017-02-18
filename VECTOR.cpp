441d4ba686186148075449c9e5530bf6df99983e
lizaKonovalova <liza_konovalova_2000234@mail.ru>
2017-02-18 23:23:52 +0300

    vectorclass
    vector

---
+++ VECTOR.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <string>
+class vector ;
+}
+     int* Array;
+     int size;
+     int cap;
+     vector(int new_size) ;
+     {
+         Array[i];
+          for(int i;i<n;i++){
+             this->Array=Array[i];
+             *Array[i]=NULL;
+        }
+     };
+     ~vector( );
+     {
+         delete *Array;
+     };
+    if(this->Array != NULL)
+		{
+			delete(this->Array, this->size);
+		}
+		this->size = 0;
+		this->Array = new a[this->size];
+ +    };
+ +    void append(int new_size, int n)
+    {
+    new_size=size*n;
+    for(i=size; i< new_size-size; i++)
+    {
+    Array[i]-> Array[i+1]=NULL;
+    }
+    void push_back ( int a) ;
+    if(size==cap
+    {
+        if (cap != 0);
+			else cap++;
+    }
+    int main () ;
